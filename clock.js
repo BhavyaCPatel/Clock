e34180e555c7f54b2099de963aef4a5604e7ab78
Bhavya Patel <88731576+BhavyaCPatel@users.noreply.github.com>
2023-08-05 22:59:58 +0530

    Add files via upload

---
+++ clock.js
@@ -0,0 +1,37 @@
+setInterval(showTime, 1000);
+function showTime() {
+        let time = new Date();
+        let hour = time.getHours();
+        let min = time.getMinutes();
+        let sec = time.getSeconds();
+        let date = time.getDate();
+        let year = time.getFullYear(); 
+        let month = time.getMonth() + 1;
+        am_pm = "AM";
+
+        if (hour > 12) {
+                hour -= 12;
+                am_pm = "PM";
+        }
+        if (hour == 0) {
+                hr = 12;
+                am_pm = "AM";
+        }
+
+        hour = hour < 10 ? "0" + hour : hour;
+        min = min < 10 ? "0" + min : min;
+        sec = sec < 10 ? "0" + sec : sec;
+
+        document.getElementById("hour")
+                .innerHTML = hour;
+        document.getElementById("minute")
+                .innerHTML = min;
+        document.getElementById("second")
+                .innerHTML = sec;
+        document.getElementById("am")
+                .innerHTML = am_pm;
+        document.getElementById("date")
+                .innerHTML = date+'/'+month+'/'+year;
+}
+showTime();
+
